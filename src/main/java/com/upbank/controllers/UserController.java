dbe6f87b7603dd59e3f564551fb3cc7e1027ef59
Gabriel Silveira <gabrielsilveiradeazevedo@gmail.com>
2024-05-06 20:29:28 -0300

    feat: TransactionController criado

--- src/main/java/com/upbank/controllers/UserController.java
+++ src/main/java/com/upbank/controllers/UserController.java
@@ -5,7 +5,6 @@ import com.upbank.dtos.UserDTO;
 import com.upbank.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/upbank/services/NotificationService.java
+++ src/main/java/com/upbank/services/NotificationService.java
@@ -2,6 +2,7 @@ package com.upbank.services;
 
 import com.upbank.domain.user.User;
 import com.upbank.dtos.NotificationDTO;
+import org.flywaydb.core.internal.util.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +18,11 @@ public class NotificationService
     @Autowired
     private RestTemplate restTemplate;
 
+
     public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
-
+/*
        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
 
        if ((notificationResponse.getStatusCode() == HttpStatus.OK))
@@ -29,5 +31,9 @@ public class NotificationService
            throw new Exception("Serviço de notificação está fora do ar");
 
        }
+
+ */
+        System.out.println("Notificação enviada com sucesso");
     }
+
 }
